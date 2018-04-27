ada1259c5a3b994416ad862c1d7fd2de16021597
Ribhi El-Zaru <ribhielzaru@gmail.com>
2018-04-27 00:18:34 -0400

    Commented out annoying code
    Commented out data collection in startup.py

--- startup.py
+++ startup.py
@@ -14,7 +14,7 @@ folder = 'data/1001Albums' #Replace with a folder of .pkl files containg Song ob
 #The line below re-creates a dataset from the RockListMusic.com list and loads them into the directory specified by the 'folder' var.
 #NOTE: this code takes a very long time to run. If you would like to try it out, we suggest running it overnight.
 #It also creates a .txt file containing info for each song, and a log file to store output.
-loaddata.loadDataFromAlbums(loaddata.getLarkin1000(folder), folder, folder + '.txt', folder + '.log')
+#loaddata.loadDataFromAlbums(loaddata.getLarkin1000(folder), folder, folder + '.txt', folder + '.log')
 
 #load songs variable with 500 Song objects, using random cluster sampling
 
